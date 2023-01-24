128754064b5fb9d4a98df158bf106ff9a73834d4
takemuraryou <foxrobotto@gmail.com>
2023-01-25 00:41:52 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+let obj = "";
+let len = 3;
+
+const chara = ['ア','イ','ウ','エ','オ','カ','キ','ク','ケ','コ','サ','シ','ス','セ','ソ','タ','チ','ツ',
+                'テ','ト','ナ','二','ヌ','ネ','ノ','ハ','ヒ','フ','ヘ','ホ','マ','ミ','ム','メ',
+                'モ','ヤ','ユ','ヨ','ラ','リ','ル','レ','ロ','ワ','ヲ','ン']
+let charaLen = chara.length;
+
+$(function(){
+    // 変数の初期化
+    let num = 0;
+    // 配列のインデックス数からランダムな数値生成
+    
+    $('#start').on('click', function(){
+
+        if(num > 5){
+        $('li').remove();
+        }
+        //numを1つ増やす
+        for(let r = 0; r<= 5; r++){
+        num++;
+        for (let i = 0; i< len; i++) {
+            obj += chara[Math.floor(Math.random()*charaLen)];
+        }
+
+        $('ul').append('<li>' + obj + '</li>');
+
+        obj = "";
+
+        }
+    });
+
+    // id属性がdeleteの要素がクリックされたら要素を削除
+    $('#delete').on('click', function(){
+        //numを1つ減らす
+        num--;
+        $('li:last').remove();
+
+        //要素が無くなったら変数を0にする
+        if(num<0){
+            num = 0;
+        }
+    });
+});
